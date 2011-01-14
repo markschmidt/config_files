68c5b85397dc2f1077eafc34c160832665154a46
Mark Schmidt <mark.schmidt@xing.com>
2011-01-14 09:52:53 +0100

    better install-script

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
-#!/bin/bash
-for file in bash_profile bashrc gitconfig irbrc vimrc gemrc
-do
-  if [ -f $HOME/.$file ]
-    then
-    echo "creating backup for $file -> ${file}.bak"
-    mv $HOME/.$file $HOME/.${file}.bak
+#!/bin/sh
+
+cutstring="DO NOT EDIT BELOW THIS LINE"
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e $target ]; then
+    if [ ! -L $target ]; then
+      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
+      if [[ -n $cutline ]]; then
+        let "cutline = $cutline - 1"
+        echo "Updating $target"
+        head -n $cutline "$target" > update_tmp
+        startline=`tail -r "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
+        if [[ -n $startline ]]; then
+          tail -n $startline "$name" >> update_tmp
+        else
+          cat "$name" >> update_tmp
+        fi
+        mv update_tmp "$target"
+      else
+        echo "WARNING: $target exists but is not a symlink."
+      fi
+    fi
+  else
+    if [[ $name != 'install.sh' ]]; then
+      echo "Creating $target"
+      if [[ -n `grep "$cutstring" "$name"` ]]; then
+        cp "$PWD/$name" "$target"
+      else
+        ln -s "$PWD/$name" "$target"
+      fi
+    fi
   fi
-  ln -s $HOME/config_files/$file $HOME/.$file
 done
